22d80e26ea45f25b22ab3dd9cd06da018af9db9f
Emanuel Calvo <3manuek@esdebian.org>
2018-07-07 01:43:19 -0300

    Added queries for pg_stat_statetments

---
+++ sql/PgStatStatements/slowest_queries.sql
@@ -0,0 +1,18 @@
+-- slowest query
+SELECT queryid,
+    query,
+    calls,
+    total_time/1000 as time_secs,
+    (total_time/1000)/calls as per_call,
+    CASE
+      WHEN (shared_blks_hit > 0 AND shared_blks_read > 0) THEN round(shared_blks_hit / (shared_blks_hit + shared_blks_read)) 
+      ELSE 0 END as hit_ratio,
+    CASE
+      WHEN (shared_blks_dirtied > 0) THEN round(shared_blks_dirtied / calls )
+      ELSE 0 END  as blk_dirt_per_call,
+    CASE
+      WHEN (shared_blks_written > 0) THEN round(shared_blks_written / calls )
+      ELSE 0 END as blk_wrtn_per_call
+FROM pg_stat_statements
+ORDER BY total_time/calls DESC
+LIMIT 10;
