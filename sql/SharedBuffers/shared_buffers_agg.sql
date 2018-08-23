710a9d2b2437bf7f976e45750f73256f3a2849f9
Emanuel Calvo <3manuek@esdebian.org>
2018-08-22 21:41:23 -0300

    Added buffercache inspect query

--- sql/SharedBuffers/shared_buffers_agg.sql
+++ sql/SharedBuffers/shared_buffers_agg.sql
@@ -1,4 +1,4 @@
 -- Aggregating the info in buffercache to have a pincture of the shared buffers usage shape.
 SELECT count(*), sum(usagecount) ,  count(case when isdirty then 1 end ), sum(pinning_backends)
 FROM pg_buffercache;
-
+select usagecount, count(*) from pg_buffercache group by 1;
