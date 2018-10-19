8610c7c91fa0d2304262aab6f3bf093ecac0b939
Emanuel Calvo <emanuel@ongres.com>
2018-10-19 16:31:15 -0300

    added settings sql

---
+++ sql/Settings/generic.sql
@@ -0,0 +1,11 @@
+-- Get a better output of pg_settings
+SELECT name, category,
+        setting,
+        short_desc::text , extra_desc::text, 
+        context,
+        sourcefile || '#L' || sourceline as varloc
+FROM pg_settings
+WHERE 
+    category ~ '^Replication'
+    -- category ~ 'eplication'
+ORDER BY context
