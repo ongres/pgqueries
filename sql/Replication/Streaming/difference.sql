255028ab1639301be03dd19e1788a4fde20e3a8e
Emanuel Calvo <emanuel@ongres.com>
2018-11-09 18:30:49 -0300

    added streaming query

---
+++ sql/Replication/Streaming/difference.sql
@@ -0,0 +1,8 @@
+-- Return the difference between de sent lsn and current lsn (the delay
+-- in the master basically)
+select pid,
+    application_name,
+    state,
+    pg_wal_lsn_diff(pg_current_wal_insert_lsn(),sent_lsn) 
+from pg_stat_replication;
+-- https://www.postgresql.org/docs/11/monitoring-stats.html#PG-STAT-REPLICATION-VIEW
