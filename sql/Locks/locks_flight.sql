7f53e5fd8fdfad8a751e0a053ad6350153c29aef
Emanuel Calvo <emanuel@ongres.com>
2019-08-13 14:40:11 +0000

    Update locks_flight.sql

--- sql/Locks/locks_flight.sql
+++ sql/Locks/locks_flight.sql
@@ -5,21 +5,23 @@ SELECT
   query,
   array_agg( distinct pg_blocking_pids(pid)) as "BlockingPids",
   count(pl.*) as "NumLocks",
-  array_agg( pl.mode) as "Modes"
+  array_agg( pl.mode) as "Modes",
+  now() - xact_start  as "Running time"
 FROM pg_stat_activity pa join pg_locks pl using (pid)
 WHERE application_name ~ 'sidekiq'
   and wait_event IS NOT NULL
-  group by pid,query 
+  group by pid,query, xact_start 
 ),
 blocking_query AS (
     SELECT pid, query, array_agg(distinct pid), 
     count(pl.*) as "NumLocks",
-    array_agg(pl.mode) as "Modes"
+    array_agg(pl.mode) as "Modes",
+    '0 seconds'::interval
     FROM pg_stat_activity pa JOIN pg_locks pl USING (pid)
     WHERE pid IN (select distinct unnest(pg_blocking_pids(pid)) FROM pg_stat_activity)
     group by pid, query
 )
-SELECT * FROM blocked_queries UNION SELECT * FROM blocking_query
+SELECT * FROM blocked_queries UNION SELECT * FROM blocking_query;
   
   
 -- gitlabhq_production=# SELECT * FROM pg_stat_activity limit 1;
