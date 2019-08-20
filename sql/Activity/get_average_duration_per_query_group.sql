934fa0be354a3e1ff1e239028295809c45b033d9
Emanuel Calvo <3manuek@gmail.com>
2019-08-20 12:26:34 -0300

    added info to query stats

--- sql/Activity/get_average_duration_per_query_group.sql
+++ sql/Activity/get_average_duration_per_query_group.sql
@@ -1,5 +1,5 @@
 -- Gets max /avg duration per usenrname or query pattern
- select count(*), avg(now()-query_start), max(now()-query_start) 
+ select usename, count(*), avg(now()-query_start), max(now()-query_start) 
    from pg_stat_activity 
    where
      1 = 1  
