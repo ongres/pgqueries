6aa3bdb4287ba0dd0b5c477b36357f6992346b77
Emanuel Calvo <3manuek@gmail.com>
2019-08-20 19:50:20 -0300

    fix

--- sql/Activity/get_average_duration_per_query_group.sql
+++ sql/Activity/get_average_duration_per_query_group.sql
@@ -1,9 +1,11 @@
 -- Gets max /avg duration per usenrname or query pattern
- select usename, count(*), avg(now()-query_start), max(now()-query_start) 
+ select usename, count(*), avg(now()-query_start), 
+   max(now()-query_start)
    from pg_stat_activity 
    where
      1 = 1  
      --
      -- and query ~ 'unicorn'
+    and state <> 'idle'
     group by usename
 ;
