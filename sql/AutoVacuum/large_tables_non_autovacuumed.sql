9ef44ea9c9c4c4d71d87fb2bbeb1f3bf018a542e
emanuel_ongres <emanuel@ongres.com>
2019-10-08 06:09:42 -0300

    Improved vacuum query

--- sql/AutoVacuum/large_tables_non_autovacuumed.sql
+++ sql/AutoVacuum/large_tables_non_autovacuumed.sql
@@ -1,6 +1,9 @@
 -- Search for very large tables with not enough writes to invoce autovacuum (threshold might be reduced if used ratio) 9.0
 
-select relname, n_dead_tup, n_dead_tup / extract('days' from now()-last_vacuum) as dead_by_day,
+select relname, 
+pg_size_pretty(pg_total_relation_size(relname::regclass)) as rank_size,
+rank() over ( order by pg_total_relation_size(relname::regclass) desc), 
+n_dead_tup, n_dead_tup / extract('days' from now()-last_vacuum) as dead_by_day,
 -- extract('days' from now()-last_vacuum) as last_vacuum, 
 extract('days' from now()-last_autovacuum) as last_autovacuum,
 -- extract('days' from now()-last_analyze) as last_analyze,
