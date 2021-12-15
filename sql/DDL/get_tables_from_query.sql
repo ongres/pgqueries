eb6d1f33a70f304f96fd6732d203d29e63b17021
Emanuel Calvo <emanuel@ongres.com>
2021-12-15 14:16:39 +0000

    Update sql/Indexes/duplicated_indexes.sql, sql/Indexes/fk_without_index.sql, sql/Indexes/unused_indexes.sql, sql/Indexes/worst_index_hits.md, sql/Indexes/worst_index_hits.sql, sql/Indexes/most_used_indexes.sql, sql/DDL/get_tables_from_query.sql

---
+++ sql/DDL/get_tables_from_query.sql
@@ -0,0 +1,11 @@
+
+-- Get the tables from a query.
+create or replace function get_query_tables(p_query text) returns text[] language plpgsql as $$
+declare
+  x xml;
+begin
+  execute 'explain (format xml) ' || p_query into x;
+  return xpath('//explain:Relation-Name/text()', x, array[array['explain', 'http://www.postgresql.org/2009/explain']])::text[];
+end $$;
+
+select get_query_tables('SELECT 1');
