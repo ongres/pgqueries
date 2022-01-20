feb427ba8f99b073c83caf005b0348e2f2626e58
Emanuel Calvo <emanuel@ongres.com>
2022-01-20 18:38:52 +0000

    Update sql/Bloat/detailed_dirty_tables.sql

--- sql/Bloat/detailed_dirty_tables.sql
+++ sql/Bloat/detailed_dirty_tables.sql
@@ -1,5 +1,9 @@
--- Get dirty rows per table 
-select schemaname, relname, n_live_tup, n_dead_tup,
+-- Get a curated list of dirty tables, filtering by those that have more than 
+-- 5% of bloat, ordered by dead tuples. 
+select schemaname || '.' || quote_ident(relname) as tablefqdn, 
+       n_live_tup, n_dead_tup,
+       round((100*n_dead_tup) / n_live_tup) as dirty_perc,
        pg_size_pretty(pg_relation_size(schemaname || '.' || quote_ident(relname))) as size
-       from pg_stat_user_tables
-       WHERE n_dead_tup > 0 order by n_dead_tup desc;
+FROM pg_stat_user_tables
+WHERE n_dead_tup > 0 AND ((100*n_dead_tup) / n_live_tup) > 5  
+ORDER BY n_dead_tup desc, dirty_perc desc;
