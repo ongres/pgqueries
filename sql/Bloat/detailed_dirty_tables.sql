322bc242245f94d85679399d880dc6de8d53d506
Emanuel Calvo <3manuek@gmail.com>
2023-06-19 12:43:06 +0000

    fix: bload detailed tables

--- sql/Bloat/detailed_dirty_tables.sql
+++ sql/Bloat/detailed_dirty_tables.sql
@@ -5,5 +5,7 @@ select schemaname || '.' || quote_ident(relname) as tablefqdn,
        round((100*n_dead_tup) / n_live_tup) as dirty_perc,
        pg_size_pretty(pg_relation_size(schemaname || '.' || quote_ident(relname))) as size
 FROM pg_stat_user_tables
-WHERE n_dead_tup > 0 AND ((100*n_dead_tup) / n_live_tup) > 5  
+WHERE n_dead_tup > 0 AND 
+      n_live_tup > 0 AND 
+      ((100*n_dead_tup) / n_live_tup) > 5  
 ORDER BY n_dead_tup desc, dirty_perc desc;
