7bb5b4b835b0cab364267ae6200ad05f2b30c61c
Diego <diego@ongres.com>
2023-06-29 14:07:38 -0300

    28cda701ca125e6f241ee990336dda48982f2ce5

--- sql/AutoVacuum/large_tables_non_autovacuumed.sql
+++ sql/AutoVacuum/large_tables_non_autovacuumed.sql
@@ -2,8 +2,8 @@
 
 select 
   relname, 
-  pg_size_pretty(pg_total_relation_size(relname::regclass)) as rank_size,
-  rank() over ( order by pg_total_relation_size(relname::regclass) desc), 
+  pg_size_pretty(pg_total_relation_size((schemaname||'.'||relname)::regclass)) as rank_size,
+  rank() over ( order by pg_total_relation_size((schemaname||'.'||relname)::regclass) desc), 
   n_dead_tup, 
   -- n_dead_tup / extract('days' from clock_timestamp()-last_vacuum) as dead_by_day,
   -- extract('days' from clock_timestamp()-last_vacuum) as last_vacuum, 
