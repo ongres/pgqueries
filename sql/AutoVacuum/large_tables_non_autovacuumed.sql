c6378267280a3a4d955f03ce715e8103a301c596
Diego <diego@ongres.com>
2023-07-03 14:26:47 -0300

    fixed quotes due non lower case objects names

--- sql/AutoVacuum/large_tables_non_autovacuumed.sql
+++ sql/AutoVacuum/large_tables_non_autovacuumed.sql
@@ -1,9 +1,9 @@
 -- Search for very large tables with not enough writes to invoce autovacuum (threshold might be reduced if used ratio) 9.0
 
-select json_agg(t) from (select
+select 
   schemaname, relname, 
-  pg_size_pretty(pg_total_relation_size((schemaname||'.'||relname)::regclass)) as rank_size,
-  rank() over ( order by pg_total_relation_size((schemaname||'.'||relname)::regclass) desc), 
+  pg_size_pretty(pg_total_relation_size(('"'||schemaname||'"."'||relname||'"')::regclass)) as rank_size,
+  rank() over ( order by pg_total_relation_size(('"'||schemaname||'"."'||relname||'"')::regclass) desc), 
   n_dead_tup, 
   -- n_dead_tup / extract('days' from clock_timestamp()-last_vacuum) as dead_by_day,
   -- extract('days' from clock_timestamp()-last_vacuum) as last_vacuum, 
@@ -15,4 +15,4 @@ select json_agg(t) from (select
   -- analyze_count,
   autoanalyze_count 
 from pg_stat_user_tables
- order by n_dead_tup desc, pg_relation_size(relid) desc) t;
+ order by n_dead_tup desc, pg_relation_size(('"'||schemaname||'"."'||relname||'"')::regclass) desc;
