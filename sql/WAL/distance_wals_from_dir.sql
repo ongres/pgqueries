c2f302743c394e67d158099cd2e2bc02cfb54d87
Diego <diego@ongres.com>
2023-07-04 09:22:14 -0300

    28cda701ca125e6f241ee990336dda48982f2ce5

--- sql/WAL/distance_wals_from_dir.sql
+++ sql/WAL/distance_wals_from_dir.sql
@@ -1,11 +1,12 @@
 -- Current amount of WALs present in the waldir
-WITH oldest_mod_walfile AS (
-    SELECT (substring(name from 9 for 8) || '/' || substring(name from 17 for 8 ))::pg_lsn as oldest_lsn
-        FROM pg_ls_waldir() 
-        ORDER BY modification DESC LIMIT 1
+with oldest_mod_walfile as (
+    select (substring(name from 9 for 8) || '/' || substring(name from 17 for 8 ))::pg_lsn as oldest_lsn
+        from pg_ls_waldir() 
+        where name not ilike 'history' /* Skip .history files */
+        order by modification desc limit 1
 )
-SELECT pg_size_pretty(pg_wal_lsn_diff( pg_current_wal_lsn(),oldest_lsn))
-FROM oldest_mod_walfile;
+select pg_size_pretty(pg_wal_lsn_diff( pg_current_wal_lsn(),oldest_lsn))
+from oldest_mod_walfile;
 
 
 
